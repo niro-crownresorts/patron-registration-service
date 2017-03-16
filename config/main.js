1f51d5c495ec5d75d592642bf277b0b6ec8aefcd
Niroshana Pathirana <niroshana.pathirana@outlook.com>
2017-03-16 22:44:29 +1100

    DB connection added

--- config/main.js
+++ config/main.js
@@ -1,6 +1,6 @@
 module.exports = {  
   // Database connection information
-  'database': 'mongodb://heroku_zhbzbh4l:u25dfj6h9jkha7hcmcaj4pl41u@ds123370.mlab.com:23370/heroku_zhbzbh4l',    
+  'database': 'mongodb://heroku_ms13kdx0:d98vam702tluo5qrul95rfmsm2@ds133340.mlab.com:33340/heroku_ms13kdx0',    
   // Setting port for server
   'port': process.env.PORT || 5000    
 }
