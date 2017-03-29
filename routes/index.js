11a4efbe2a44c0ef0225d113f8c6d91c9791453d
Niroshana Pathirana <Niroshana.Pathirana@crownresorts.com.au>
2017-03-30 07:54:49 +1100

    Updated for patron

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Realistic Cloud Solution' });
+  res.render('index', { title: 'Crown Resorts' });
 });
 
 module.exports = router;
